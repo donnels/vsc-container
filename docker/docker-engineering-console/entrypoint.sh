4b0af7ae6bbf6f3c5bb6c49e882afadb1329c9c2
Sean Donnellan <gitddonnellan.de>
2025-08-20 00:48:00 +0200

    engineering console running

--- docker/docker-engineering-console/entrypoint.sh
+++ docker/docker-engineering-console/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+git config --global user.email "git@donnellan.de"
+git config --global user.name "Sean Donnellan"
 exec /usr/bin/code-server /home/coder/code \
     --bind-addr 0.0.0.0:8443 \
     --auth password \
-    --password ${PASSWORD} \
     --cert /etc/letsencrypt/live/${CERT_NAME}/fullchain.pem \
     --cert-key /etc/letsencrypt/live/${CERT_NAME}/privkey.pem \
     --welcome-text "${CODE_SERVER_AUTH_MESSAGE}"
